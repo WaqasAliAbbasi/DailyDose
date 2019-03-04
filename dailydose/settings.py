99eff5cc06d63aac907b81f189460538a095169e
Waqas Ali <waqas.abbasi@outlook.com>
2019-03-04 18:04:45 +0800

    both: prepare for heroku deploy

--- dailydose/settings.py
+++ dailydose/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ WEBPACK_LOADER = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
 }
+
+django_heroku.settings(locals())
